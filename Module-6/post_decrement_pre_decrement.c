432b9ce780bc340f8a585a7f8e0967d17d98099b
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-14 20:10:04 +0600

    learn: pre and post increment and decrement

---
+++ Module-6/post_decrement_pre_decrement.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int x = 10;
+    int y = x--; // post increment : y = x, x = x + 1 -> actions performs at first then increment
+    printf("%d %d\n", x, y);
+    int z = --x; // pre increment : x = x + 1, z = x -> increment performs at first then actions
+    printf("%d %d", x, z);
+
+    return 0;
+}
