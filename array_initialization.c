3ad4bd0f75402e6706579d392c1db887686e41f7
Rahad-Ullah <rahadullah10@gmail.com>
2025-10-17 18:50:31 +0600

    sum of all values of array

---
+++ array_initialization.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int arr[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+    for (int i = 0; i < 20; i++)
+    {
+        printf("%d \n", arr[i]);
+    }
+
+    return 0;
+}
